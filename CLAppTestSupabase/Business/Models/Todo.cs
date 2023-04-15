43f281e17ad86f620e50a4e924d0cf5832b44388
B45T13N <bastds.dev@gmail.com>
2023-04-15 13:38:00 +0200

    Création des classes

---
+++ CLAppTestSupabase/Business/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CLAppTestSupabase.Business.Models;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string Note { get; set; }
+    public string Title { get; set; }
+    public DateTime CreationDate { get; set; }
+}
---
+++ CLAppTestSupabase/Data/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Postgrest.Attributes;
+using Postgrest.Models;
+
+namespace CLAppTestSupabase.Data.Models
+{
+    [Table("todos")]
+    public class Todo : BaseModel
+    {
+        [PrimaryKey("id")]
+        public int Id { get; set; }
+        [Column("note")]
+        public string Note { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+        [Column("created_at")]
+        public DateTime CreationDate { get; set; }
+    }
+}
