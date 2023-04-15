2a4bf203dc0c2d1e8f145ecdffebf76238bfd1b4
B45T13N <bastds.dev@gmail.com>
2023-04-15 13:38:25 +0200

    Interface creation

---
+++ CLAppTestSupabase/Business/Interfaces/ITodoService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CLAppTestSupabase.Business.Models;
+
+namespace CLAppTestSupabase.Business.Interfaces;
+
+public interface ITodoService
+{
+    Task<IEnumerable<Todo>> GetTodos();
+    Task<Todo> GetTodo(int id);
+    Task<Todo> InsertTodo(Todo todo);
+}
