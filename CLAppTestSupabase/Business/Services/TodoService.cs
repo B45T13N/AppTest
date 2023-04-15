97cdd61a9f77f22f08c4952dab1d7300e058f6f0
B45T13N <bastds.dev@gmail.com>
2023-04-15 13:38:59 +0200

    Interface implementation

---
+++ CLAppTestSupabase/Business/Services/TodoService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CLAppTestSupabase.Business.Interfaces;
+using CLAppTestSupabase.Business.Mappers;
+using CLAppTestSupabase.Business.Models;
+using CLAppTestSupabase.Data;
+using Postgrest;
+using Client = Supabase.Client;
+
+namespace CLAppTestSupabase.Business.Services;
+
+public class TodoService : ITodoService
+{
+    public Client SupabaseClient { get; set; }
+    
+    public TodoService(string url, string key)
+    {
+         SupabaseClient = SupabaseConnexion.GetConnexion(url, key);
+    }
+    
+    public async Task<IEnumerable<Todo>> GetTodos()
+    {
+        var todos = await SupabaseClient
+            .From<Data.Models.Todo>()
+            .Get();
+
+        return todos.Models.ToModel();
+    }
+
+    public async Task<Todo> GetTodo(int id)
+    {
+        var todo = await SupabaseClient
+            .From<Data.Models.Todo>()
+            .Filter("id", Constants.Operator.Equals, id)
+            .Get();
+
+        return todo.Models[0].ToModel();
+    }
+
+    public async Task<Todo> InsertTodo(Todo todo)
+    {
+        var newTodo = await SupabaseClient
+            .From<Data.Models.Todo>()
+            .Insert(todo.ToEntity());
+
+        return newTodo.Models[0].ToModel();
+    }
+}
