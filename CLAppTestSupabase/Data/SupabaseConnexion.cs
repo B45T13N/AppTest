a73cacc81b6860f4d727e95fef84c73e5326fefa
B45T13N <bastds.dev@gmail.com>
2023-04-15 13:38:07 +0200

    SupabaseConnexion Init

---
+++ CLAppTestSupabase/Data/SupabaseConnexion.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Policy;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Supabase;
+
+namespace CLAppTestSupabase.Data;
+
+public class SupabaseConnexion
+{
+    public static Client GetConnexion(string url, string key)
+    {
+        var options = new Supabase.SupabaseOptions
+        {
+            AutoConnectRealtime = true
+        };
+
+        var supabase = new Supabase.Client(url, key, options);
+
+        return supabase;
+    }
+}
