37ba05b0f4177c6f9b2200cfba62462c69a41793
B45T13N <bastds.dev@gmail.com>
2023-04-15 13:38:15 +0200

    SupabaseConnexion Init

--- CLAppTestSupabase/Program.cs
+++ CLAppTestSupabase/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace CLAppTestSupabase
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CLAppTestSupabase.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Supabase;
+
+namespace CLAppTestSupabase
 {
-    internal class Program
+    class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            // Setup Host
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+            IConfiguration config = builder.Build();
+            
+            Console.WriteLine("Welcome to the Supabase CL Test");
+            Console.WriteLine("This project is made to test Supabase Fonctionnality");
+            
+            Console.WriteLine($"{Environment.NewLine} To read todos registered, please press 1,");
+            Console.WriteLine("To register a new todo, please press 2");
+
+            var keyPressed = Console.ReadLine();
+
+            Console.WriteLine(keyPressed);
         }
+
     }
 }
